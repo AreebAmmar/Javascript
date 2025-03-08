c40d0526a74df16fea6d6e78f89e6a69e975e560
Areeb Ammar <areebammarr@gmail.com>
2025-03-08 16:08:01 +0530

    first commit

---
+++ Chapter1_Practiceset.js
@@ -0,0 +1,28 @@
+// chap1 q1&q2
+// let a="Zeeshan"
+// let b=1
+// console.log(a+b)
+// console.log(typeof (a+b))
+
+
+// q3
+// const obj={name:"areeb",
+//     age:22
+// }
+// console.log(obj)
+// obj="array"
+
+
+//q4
+// obj['place']="vaniyambadi"
+// console.log(obj)
+
+
+
+// const dict={
+//     appreciate:"worth",
+//     ataraxia:"emotional",
+//     yakka:"hardwork"
+// }
+// console.log(dict['yakka'])
+// console.log(dict.ataraxia)
