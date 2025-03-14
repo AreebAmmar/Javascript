56051eb33087042e7283a888c97b7e47a876ddad
Areeb Ammar <areebammarr@gmail.com>
2025-03-14 11:16:02 +0530

    Commit

--- Chapter3_Practiceset.js
+++ Chapter3_Practiceset.js
@@ -1,24 +1,53 @@
-const readline = require("readline-sync");
-let n=readline.question("Enter Number");
-// For Loop
-let sum=0
-for (let i = 0; i < n; i++) {
-    sum+=(i+1)
+const prompt = require("prompt-sync")();
+
+let a = function(a,b,c,d){
+   return(a,b,c,d/2) 
 }
-console.log("Sum for the",n,"is",sum)
+// console.log(mean(1,2,3,4));
+console.log(a(1,2,3,4)/4);
+
+// let num=4
+// let i
+// while(i!=num){
+//     i= prompt("Enter Number");   
+//     console.log("Try Again") 
+// }
+// console.log("You are correct")
+
+// problem1and2
+// const obj ={
+//     "Areeb":90,
+//     "Zeeshu":99,
+//     "Faheem":96
+// }
+// for (let i=0;i<Object.keys(obj).length;i++){
+//     console.log("The marks of",Object.keys(obj)[i],"is",obj[Object.keys(obj)[i]])
+// }
+// for (let a in obj){
+//     console.log("The marks of",a)
+// }
+
+
+// For Loop
+// let sum=0
+// for (let i = 0; i < n; i++) {
+//     sum+=(i+1)
+// }
+// console.log("Sum for the",n,"is",sum)
 
 
 // while loop
-i=0
-while(i<n){
-    console.log(i)
-    i++
-}
+// i=0
+// while(i<n){
+//     console.log(i)
+//     i++
+// }
 
 
 // do while loop
-i=0
-do{
-console.log(i)
-i++
-}while(i<n)
+// i=0
+// do{
+// console.log(i)
+// i++
+// }while(i<n)
+
---
+++ Chapter4_PracticeSet.js
@@ -0,0 +1,20 @@
+let a = "Fah\ner"
+console.log(a.length);
+
+
+
+const sen="Zeeshu is my baby boy"
+const Areeb="boy"
+console.log("boy",sen.includes(Areeb)?"is":"is not","in the sentence")
+
+
+
+let z="zeeshu"
+console.log(z.toUpperCase())
+
+
+let y="Please Give 1000"
+console.log(y.slice(12))
+
+
+// pb5 cant possible
